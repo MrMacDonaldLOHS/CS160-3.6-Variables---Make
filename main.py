af6751f886af1e54508de975478dc694fa4b8109
mrmacdonald <96990486+MrMacDonaldLOHS@users.noreply.github.com>
2022-02-21 17:17:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Variable Assignment
+
+# Assign your name and your favourite food to 2 separate variables with suitable names.
+
+############# SIMPLE ###################
+# Output the contents of the variables on 2 separate lines
+
+############# MEDIUM ####################
+# Using the variables, output two sentences (not just the contents of the variables). The first with your name, the second with your favourite food.
+
+############# COMPLEX ###################
+# Usng the variables Output both pieces of information as part of the same sentence.
+# Make sure that you have spaces and punctuation in the correct places. 
